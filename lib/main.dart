773eeb7852661bee7791cc5d1b7b459770937869
bong <darksen7693@gmail.com>
2023-10-08 15:56:33 +0900

    최초 세팅완료

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:netflix/theme.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "BongFlix",
+      theme: theme(),
+      home: DefaultTabController(
+        length: 4,
+        child: Scaffold(
+          body: TabBarView(
+            physics: NeverScrollableScrollPhysics(),
+            children: <Widget>[
+              Container(),
+              Container(),
+              Container(),
+              Container(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
