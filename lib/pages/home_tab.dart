afae68e8645c6b1ad8e189c0aebd586834bc3372
bong <darksen7693@gmail.com>
2023-10-08 17:54:28 +0900

    더미 이미지 로딩완료

--- lib/pages/home_tab.dart
+++ lib/pages/home_tab.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix/components/top_bar.dart';
 import 'package:netflix/model/model_movie.dart';
@@ -12,10 +13,10 @@ class HomeTab extends StatefulWidget {
 class _HomeTabState extends State<HomeTab> {
   List<Movie> movies = [
     Movie.fromMap({
-      "title": "사랑의 불시착",
-      "keyword": "사랑/로맨스/판타지",
-      "poster": "test_movie_1.png",
-      "like": false
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'assets/test_movie_1.png',
+      'like': false
     })
   ];
 
@@ -26,6 +27,63 @@ class _HomeTabState extends State<HomeTab> {
 
   @override
   Widget build(BuildContext context) {
-    return TopBar();
+    return ListView(children: <Widget>[
+      Stack(children: <Widget>[
+        CarouselImage(movies: movies),
+        TopBar(),
+      ]),
+    ]);
+  }
+}
+
+class CarouselImage extends StatefulWidget {
+  final List<Movie> movies;
+  CarouselImage({required this.movies});
+
+  @override
+  State<CarouselImage> createState() => _CarouselImageState();
+}
+
+class _CarouselImageState extends State<CarouselImage> {
+  late List<Movie> movies;
+  late List<Widget> images;
+  late List<String> keywords;
+  late List<bool> likes;
+  int _currentPage = 0;
+  late String _currentKeyword;
+
+  @override
+  void initState() {
+    super.initState();
+    movies = widget.movies;
+    images = movies.map((m) => Image.asset(m.poster)).toList();
+    keywords = movies.map((m) => m.keyword).toList();
+    likes = movies.map((m) => m.like).toList();
+    _currentKeyword = keywords[0];
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+          ),
+          CarouselSlider(
+            items: images,
+            options: CarouselOptions(onPageChanged: (index, reason) {
+              setState(() {
+                _currentPage = index;
+                _currentKeyword = keywords[_currentPage];
+              });
+            }),
+          ),
+          Container(
+            child: Text(_currentKeyword),
+          )
+        ],
+      ),
+    );
   }
 }
