3d5d52f01387fdcb5f8a1d8272730a6600b98b58
bong <darksen7693@gmail.com>
2023-10-08 18:28:32 +0900

    더미 추가후 슬라이딩 확인완료

--- lib/pages/home_tab.dart
+++ lib/pages/home_tab.dart
@@ -1,5 +1,6 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:netflix/components/carousel_image.dart';
 import 'package:netflix/components/top_bar.dart';
 import 'package:netflix/model/model_movie.dart';
 
@@ -17,7 +18,25 @@ class _HomeTabState extends State<HomeTab> {
       'keyword': '사랑/로맨스/판타지',
       'poster': 'assets/test_movie_1.png',
       'like': false
-    })
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'assets/test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'assets/test_movie_1.png',
+      'like': false
+    }),
+    Movie.fromMap({
+      'title': '사랑의 불시착',
+      'keyword': '사랑/로맨스/판타지',
+      'poster': 'assets/test_movie_1.png',
+      'like': false
+    }),
   ];
 
   @override
@@ -33,57 +52,5 @@ class _HomeTabState extends State<HomeTab> {
         TopBar(),
       ]),
     ]);
-  }
-}
-
-class CarouselImage extends StatefulWidget {
-  final List<Movie> movies;
-  CarouselImage({required this.movies});
-
-  @override
-  State<CarouselImage> createState() => _CarouselImageState();
-}
-
-class _CarouselImageState extends State<CarouselImage> {
-  late List<Movie> movies;
-  late List<Widget> images;
-  late List<String> keywords;
-  late List<bool> likes;
-  int _currentPage = 0;
-  late String _currentKeyword;
-
-  @override
-  void initState() {
-    super.initState();
-    movies = widget.movies;
-    images = movies.map((m) => Image.asset(m.poster)).toList();
-    keywords = movies.map((m) => m.keyword).toList();
-    likes = movies.map((m) => m.like).toList();
-    _currentKeyword = keywords[0];
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: <Widget>[
-          Container(
-            padding: EdgeInsets.all(20),
-          ),
-          CarouselSlider(
-            items: images,
-            options: CarouselOptions(onPageChanged: (index, reason) {
-              setState(() {
-                _currentPage = index;
-                _currentKeyword = keywords[_currentPage];
-              });
-            }),
-          ),
-          Container(
-            child: Text(_currentKeyword),
-          )
-        ],
-      ),
-    );
   }
 }
