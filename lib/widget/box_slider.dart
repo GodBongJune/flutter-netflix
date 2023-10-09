760790081571af8e99fe1b8659794efcf4506204
bong <darksen7693@gmail.com>
2023-10-09 18:04:55 +0900

    box,circle 상세보기 완료

--- lib/widget/box_slider.dart
+++ lib/widget/box_slider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/model/model_movie.dart';
+import 'package:netflix/pages/detail_page.dart';
+import 'package:netflix/pages/home_tab.dart';
 
 class BoxSlider extends StatelessWidget {
   late final List<Movie> movies;
@@ -18,7 +20,7 @@ class BoxSlider extends StatelessWidget {
             height: 120,
             child: ListView(
               scrollDirection: Axis.horizontal,
-              children: makeBoxImages(movies),
+              children: makeBoxImages(context, movies),
             ),
           )
         ],
@@ -27,12 +29,23 @@ class BoxSlider extends StatelessWidget {
   }
 }
 
-List<Widget> makeBoxImages(List<Movie> movies) {
+List<Widget> makeBoxImages(BuildContext context, List<Movie> movies) {
   List<Widget> results = [];
   for (var i = 0; i < movies.length; i++) {
     results.add(
       InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute<Null>(
+              fullscreenDialog: true,
+              builder: (BuildContext context) {
+                return DeatailScreen(
+                  movie: movies[i],
+                );
+              },
+            ),
+          );
+        },
         child: Container(
           padding: EdgeInsets.only(right: 10),
           child: Align(
--- lib/widget/circle_slider.dart
+++ lib/widget/circle_slider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:netflix/model/model_movie.dart';
+import 'package:netflix/pages/detail_page.dart';
+import 'package:netflix/pages/home_tab.dart';
 
 class CircleSlider extends StatelessWidget {
   late final List<Movie> movies;
@@ -18,7 +20,7 @@ class CircleSlider extends StatelessWidget {
             height: 120,
             child: ListView(
               scrollDirection: Axis.horizontal,
-              children: makeCircleImages(movies),
+              children: makeCircleImages(context, movies),
             ),
           ),
         ],
@@ -27,12 +29,23 @@ class CircleSlider extends StatelessWidget {
   }
 }
 
-List<Widget> makeCircleImages(List<Movie> movies) {
+List<Widget> makeCircleImages(BuildContext context, List<Movie> movies) {
   List<Widget> results = [];
   for (var i = 0; i < movies.length; i++) {
     results.add(
       InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context).push(
+            MaterialPageRoute<Null>(
+              fullscreenDialog: true,
+              builder: (BuildContext context) {
+                return DeatailScreen(
+                  movie: movies[i],
+                );
+              },
+            ),
+          );
+        },
         child: Container(
           padding: EdgeInsets.only(right: 10),
           child: Align(
