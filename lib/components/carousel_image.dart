1632b2c566bc7b0084108c4ce6cc3438d7dad88b
bong <darksen7693@gmail.com>
2023-10-09 17:13:49 +0900

    상세보기 poster 블러처리 완료

--- lib/components/carousel_image.dart
+++ lib/components/carousel_image.dart
@@ -1,6 +1,7 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix/model/model_movie.dart';
+import 'package:netflix/pages/home_tab.dart';
 
 class CarouselImage extends StatefulWidget {
   final List<Movie> movies;
@@ -87,7 +88,21 @@ class _CarouselImageState extends State<CarouselImage> {
                   padding: EdgeInsets.only(right: 10),
                   child: Column(
                     children: <Widget>[
-                      IconButton(icon: Icon(Icons.info), onPressed: () {}),
+                      IconButton(
+                        icon: Icon(Icons.info),
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute<Null>(
+                              fullscreenDialog: true,
+                              builder: (BuildContext context) {
+                                return DeatailScreen(
+                                  movie: movies[_currentPage],
+                                );
+                              },
+                            ),
+                          );
+                        },
+                      ),
                       Text("정보", style: TextStyle(fontSize: 11))
                     ],
                   ),
--- lib/pages/home_tab.dart
+++ lib/pages/home_tab.dart
@@ -1,3 +1,4 @@
+import 'dart:ui';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:netflix/components/carousel_image.dart';
@@ -61,4 +62,74 @@ class _HomeTabState extends State<HomeTab> {
       ],
     );
   }
+}
+
+class DeatailScreen extends StatefulWidget {
+  late final Movie movie;
+
+  DeatailScreen({required this.movie});
+
+  @override
+  State<DeatailScreen> createState() => _DeatailScreenState();
+}
+
+class _DeatailScreenState extends State<DeatailScreen> {
+  bool like = false;
+  @override
+  void initState() {
+    super.initState();
+    like = widget.movie.like;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: SafeArea(
+          child: ListView(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  Container(
+                    width: double.maxFinite,
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: AssetImage(widget.movie.poster),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: ClipRect(
+                      child: BackdropFilter(
+                        filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                        child: Container(
+                          alignment: Alignment.center,
+                          color: Colors.black.withOpacity(0.1),
+                          child: Container(
+                            child: Column(
+                              children: <Widget>[
+                                Container(
+                                  padding: EdgeInsets.fromLTRB(0, 45, 0, 10),
+                                  child: Image.asset(widget.movie.poster),
+                                  height: 300,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              makeMenuButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget makeMenuButton() {
+  return Container();
 }
